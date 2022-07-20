129f775dd1cb989a358a5599b579d7904a1b380f
Senga Ngabo Alexis <55744981+alexissengangabo07@users.noreply.github.com>
2022-07-20 12:12:44 +0200

    Update app.js

--- 8/app.js
+++ 8/app.js
@@ -1,1 +1,25 @@
-console.log(`HELLO WORLD!`);
+const button = document.querySelector('button')
+const result = document.querySelector('#Result')
+
+button.addEventListener('click', () => {
+    // Create an AJAX request
+
+    let xhr = new XMLHttpRequest();
+
+    // Prepare the Request
+
+    xhr.open('GET', '../data/developers.json', true);
+
+    // Send Request
+
+    xhr.send();
+
+    // Process the Request
+
+    xhr.onload = () => {
+        if (xhr.status === 200) {
+            console.log('success');
+        }
+    }
+
+}) 
