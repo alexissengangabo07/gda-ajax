25cd643387cf9d5900db2df335f6e952d69c17bd
Shako Kinyamba <shakokinyamba201@gmail.com>
2022-07-20 16:41:03 +0200

    finish task

--- 1/app.js
+++ 1/app.js
@@ -1,1 +1,36 @@
-console.log(`HELLO WORLD!`);
+const form=document.querySelector('.res')
+
+fetch("./developers.json",{
+    method:'GET'
+})
+.then(reponse=>{
+    if(reponse.status===200){
+        return reponse.json()
+    }
+})
+.then (reponse=>{
+    let filtredData = reponse.sort((a,b)=> a.first_name<b.first_name ? -1 : 1);
+    filtredData.forEach((element,index) => { 
+        if(Age(reponse[index].birthday)<18){
+            
+            const nom=document.createElement('td');
+            nom.textContent=reponse[index].first_name;
+            form.appendChild(nom);
+
+            const age=document.createElement('td');
+            age.textContent=Age(reponse[index].birthday);
+            form.appendChild(age);
+         
+    }
+    });
+})
+
+// const dates=new Date(reponse.birthday).toLocaleDateString();
+//         console.log(dates)
+
+function Age(year)
+{
+  birthday = new Date(year);
+  return new Number((new Date().getTime() - birthday.getTime()) / 31536000000).toFixed(0);
+}
+
