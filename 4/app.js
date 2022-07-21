cc3e5f333428413fbd620bceb1b4667954c34a90
Bonheur Bushiri <bonheurbushiri@gmail.com>
2022-07-21 16:49:58 +0200

    Begin ajax

--- 4/app.js
+++ 4/app.js
@@ -1,1 +1,24 @@
-console.log(`HELLO WORLD!`);
+
+
+
+fetch("../data/developers.json")
+    .then(reponse => reponse.json()
+    )
+    .then((tableau) => {
+let tab=tableau;
+for (let i = 0; i < tab.length; i++) {
+    if (tab[i].released_projects >10) {
+        console.log(tab[i].first_name); 
+        let afficheTab = document.createElement('tbody');
+        afficheTab.innerHTML = ` <tr>
+        <td data-th="Nom" id="nom">${tab[i].first_name}</td>
+        <td  data-th="POST-NOM" id="post">${tab[i].last_name}</td>
+        <td  data-th="PROJET" id="projet">${tab[i].released_projects}</td>
+    </tr>`;
+    document.querySelector('.mytab').appendChild(afficheTab);
+    }
+   
+}
+
+    })
+    
